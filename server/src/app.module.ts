6c44fce2b72d07be714ccdc6d4b46bd40d7c1b96
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-08 09:44:56 -0500

    init 🎉: add init config for typeorm and postgres db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: envs.POSTGRES_HOST,
+      port: envs.POSTGRES_PORT,
+      username: envs.POSTGRES_USER,
+      password: envs.POSTGRES_PASSWORD,
+      database: envs.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
