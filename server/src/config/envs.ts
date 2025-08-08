2e1fa03f01dec83ea3562abf5ef0cbc010ea1cba
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-08 09:42:07 -0500

    feat: ✨ add env configuration

---
+++ server/src/config/envs.ts
@@ -0,0 +1,40 @@
+import * as Joi from 'joi';
+import * as dotenv from 'dotenv';
+
+// Cargar variables del archivo .env si no están en process.env
+dotenv.config();
+
+interface EnvVars {
+  POSTGRES_PORT: number;
+  POSTGRES_HOST: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+  POSTGRES_DB: string;
+}
+
+// Definir el esquema de validación
+const envsSchema = Joi.object<EnvVars>({
+  POSTGRES_PORT: Joi.number().default(5432),
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
+}).unknown(true); // Permite variables adicionales en `process.env`
+
+// Validar las variables de entorno
+const validationResult = envsSchema.validate(process.env);
+
+if (validationResult.error) {
+  throw new Error(`Config validation error: ${validationResult.error.message}`);
+}
+
+// Extraer los valores validados
+const envVars: EnvVars = validationResult.value;
+
+export const envs = {
+  POSTGRES_HOST: envVars.POSTGRES_HOST,
+  POSTGRES_PORT: envVars.POSTGRES_PORT,
+  POSTGRES_USER: envVars.POSTGRES_USER,
+  POSTGRES_PASSWORD: envVars.POSTGRES_PASSWORD,
+  POSTGRES_DB: envVars.POSTGRES_DB,
+};
