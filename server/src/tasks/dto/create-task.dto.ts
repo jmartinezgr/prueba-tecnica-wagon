1c09edd5131f19591989aaec43e1fe722f465b5d
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-08 23:06:14 -0500

    feat: ✨ set the DTO's for task module

--- server/src/tasks/dto/create-task.dto.ts
+++ server/src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,45 @@
-export class CreateTaskDto {}
+import {
+  IsString,
+  IsOptional,
+  IsBoolean,
+  IsArray,
+  IsDateString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class SubTaskDto {
+  @IsString()
+  title: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isCompleted?: boolean = false;
+}
+
+export class CreateTaskDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsDateString()
+  @IsOptional()
+  estimatedDate?: Date;
+
+  @IsBoolean()
+  @IsOptional()
+  isCompleted?: boolean;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => SubTaskDto)
+  @IsOptional()
+  subTasks?: SubTaskDto[];
+
+  @IsString()
+  @IsOptional()
+  repeat?: string;
+}
