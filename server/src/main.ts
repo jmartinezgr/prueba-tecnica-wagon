0395b900c38a2674eceac5f24b080948cee09fee
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-09 17:11:25 -0500

    fix: 🔨 update the server to listen in the process port for azure deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from './app.module';
  * - Creates the main application instance using the root AppModule.
  * - Sets a global API prefix for all routes (api/v1).
  * - Applies a global validation pipe to enforce DTO validation and transformation.
- * - Starts the HTTP server on port 3000.
+ * - Starts the HTTP server on the port specified by Azure or defaults to 3000.
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,7 +23,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on port ${port}`);
 }
+
 // Start the application by calling the bootstrap function
 void bootstrap();
