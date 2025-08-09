72537593a05cda40a1b96b55656a4ed943fb9423
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-08-08 19:23:07 -0500

    refactor: ♻️ enhance better logic for real user registration

--- mobile/lib/features/auth/presentation/register_screen.dart
+++ mobile/lib/features/auth/presentation/register_screen.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mobile/shared/services/api_service.dart' show ApiService;
+import 'package:mobile/shared/services/secure_storage_service.dart';
+import 'package:mobile/shared/services/shared_preferences.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -14,6 +21,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  final SecureStorageService secureStorageService = SecureStorageService();
+  final UserInfoService userInfoService = UserInfoService();
+
   bool _isLoading = false;
 
   Future<void> _register(BuildContext context) async {
@@ -21,16 +31,49 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
     setState(() => _isLoading = true);
 
-    // TODO: Cambia esto por tu lógica de endpoint y API
-    await Future.delayed(const Duration(seconds: 2));
+    ApiService apiService = ApiService();
+
+    final response = await apiService.post('auth/register', {
+      'name': _nameController.text.trim(),
+      'email': _emailController.text.trim(),
+      'password': _passwordController.text.trim(),
+    });
 
     setState(() => _isLoading = false);
+    if (response.statusCode == 201) {
+      final body = jsonDecode(response.body);
+      final accessToken = body['accessToken'];
+      final refreshToken = body['refreshToken'];
 
-    // Ejemplo de navegación después del registro
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(const SnackBar(content: Text('Registro exitoso')));
-    context.go('/login');
+      if (accessToken != null && refreshToken != null) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text('Registro exitoso')));
+        await secureStorageService.saveAccess(accessToken);
+        await secureStorageService.saveRefresh(refreshToken);
+        body.remove('accessToken');
+        body.remove('refreshToken');
+
+        // Guardamos solo la info del usuario
+        await userInfoService.saveUser(body);
+        context.go('/home');
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Error: Tokens no válidos')),
+        );
+      }
+    } else {
+      final body = jsonDecode(response.body);
+      if (body['message'] is List) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: ${body['message'].join(', ')}')),
+        );
+      } else if (body['message'] is String) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Error: ${body['message']}')));
+      }
+    }
   }
 
   @override
@@ -53,6 +96,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 // Campo Nombre
                 TextFormField(
                   controller: _nameController,
+                  keyboardType: TextInputType.text,
+                  enableSuggestions: false,
+                  autocorrect: false,
                   decoration: const InputDecoration(
                     labelText: 'Nombre',
                     border: OutlineInputBorder(),
@@ -62,24 +108,19 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       : null,
                 ),
                 const SizedBox(height: 16),
-
                 // Campo Email
                 TextFormField(
                   controller: _emailController,
+                  keyboardType: TextInputType.text,
+                  enableSuggestions: false,
+                  autocorrect: false,
                   decoration: const InputDecoration(
                     labelText: 'Correo electrónico',
                     border: OutlineInputBorder(),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Ingresa tu correo';
-                    }
-                    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
-                    if (!emailRegex.hasMatch(value)) {
-                      return 'Correo no válido';
-                    }
-                    return null;
-                  },
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Ingrese su email'
+                      : null,
                 ),
                 const SizedBox(height: 16),
 
